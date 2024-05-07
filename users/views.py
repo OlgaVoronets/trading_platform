0b39c97e230986e40eb01eaa6ae4560ab48777db
olga voronets <voronets80@gmail.com>
2024-05-07 19:25:03 +0300

    Работа с моделью пользователя, добавлены базовые серивлизатор и контроллер на создание пользователя

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
 
-# Create your views here.
+from users.models import User
+from users.serializers import UserSerializer
+
+
+class UserCreateView(generics.CreateAPIView):
+    """Регистрация нового пользователя"""
+    serializer_class = UserSerializer
